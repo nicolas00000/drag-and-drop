41ef1205c39494fe279241b4727e68dc59b01a20
Nicolas Mariano <nicmariano10@gmail.com>
2022-05-09 18:23:04 -0300

    create Li and remove

---
+++ script.js
@@ -0,0 +1,30 @@
+
+ let contador = 1 
+function adicionar(elemento){
+
+    
+
+    const ulId = elemento.previousElementSibling.id
+    const tarefa = prompt("qual a tarefa ?")
+    const board = document.getElementById(ulId)
+      
+
+const template = `
+
+<li id="${contador++}">   ${tarefa}    <p  onclick="remover(this)" >X</p>     </li>
+
+
+`
+
+console.log(template)
+
+board.innerHTML += template
+}
+
+
+
+
+function remover(elemento){
+   document.getElementById(elemento.parentElement.id).remove()  
+    
+}
