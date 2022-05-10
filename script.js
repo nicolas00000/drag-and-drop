72de8c9898f1270b1c9714eeff9e9764c66467fd
Nicolas Mariano <nicmariano10@gmail.com>
2022-05-10 16:31:14 -0300

    arrastar e soltar em todos

--- script.js
+++ script.js
@@ -1,17 +1,23 @@
 let contador = 1 
+const input = document.getElementById("nome");
 
 
-function drag(){ }
+function drag(event){
+    event.dataTransfer.setData("card", event.target.id)
+ }
 
-function over() { }
+function over(event) {
+    event.preventDefault()
+ }
 
-function drop() {}
-
-
-
-
-
-
+function drop(event, id) {
+    event.preventDefault();
+    const target = document.getElementById(id);
+    const data = event.dataTransfer.getData("card");
+    const card = document.getElementById(data);
+    target.appendChild(card);
+    event.dataTransfer.clearData();
+}
 
 
 
@@ -20,21 +26,23 @@ function adicionar(elemento){
     const ulId = elemento.previousElementSibling.id
     const tarefa = prompt("qual a tarefa ?")
     const board = document.getElementById(ulId)
-      
+    
 
+if(tarefa !== ""){
+    
 const template = `
 
-<li id="${contador++}">   ${tarefa}    <p  onclick="remover(this)" >X</p>     </li>
+<li id="${contador++}" draggable="true" ondragstart="drag(event)">   ${tarefa}    <p  onclick="remover(this)" >X</p>     </li>
 
 
 `
 
-console.log(template)
+// console.log(template)
 
 board.innerHTML += template
 }
 
-
+}
 
 
 function remover(elemento){
